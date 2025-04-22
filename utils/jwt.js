328b3cdf7974b340fa5a909d0ae5465f55117d0e
RectangleEquals <RectangleEquals@gmail.com>
2025-04-21 17:58:38 -0700

    Potentially fixed

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,34 +5,33 @@ function generateToken(payload) {
 }
 
 function authenticateToken(db) {
-    return async function (req, res, next)
-    {
-        if(!db)
-            return res.json({error: "Invalid Connection"});
+    return async function (req, res, next) {
+        if (!db)
+            return res.json({ error: 'Invalid Connection' });
 
-        if(!req.params || !req.params.authCode)
-            return res.json({error: "Bad Request"});
+        if (!req.params || !req.params.authCode)
+            return res.json({ error: 'Bad Request' });
 
         const authCode = req.params.authCode;
         const user = await db.findUserWithCode(authCode);
-        if(!user)
-            return res.json({error: "Bad User"});
-        
-        req.user = user;
-        next();
-        //return res.json({userName: user.userName, refreshToken: user.refreshToken});
-        /*
-        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (!user)
+            return res.json({ error: 'Unknown User' });
+
+        await jwt.verify(user.refreshToken, process.env.JWT_SECRET, (err, user) => {
             console.log('Verifying token...');
             if (err) {
                 console.error('Invalid Token!');
-                return res.json({error: "Invalid Token"});
+                return res.json({ error: 'Invalid Token' });
             }
             console.log('Authorized!');
-            req.user = user;
+            req.user = {
+                discordId: user.discordId,
+                userName: user.userName,
+                avatarUrl: user.avatarUrl,
+                refreshToken: user.refreshToken,
+            };
             next();
         });
-        */
     };
 }
 
